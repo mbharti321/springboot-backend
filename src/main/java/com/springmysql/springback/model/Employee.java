ca20f798ee8c6341a14644d03c1c70e10fa3a18c
Manish Bharti <mbharti321@gmail.com>
2022-09-13 13:23:45 +0530

    update employee.java

--- src/main/java/com/springmysql/springback/model/Employee.java
+++ src/main/java/com/springmysql/springback/model/Employee.java
@@ -1,6 +1,21 @@
 package com.springmysql.springback.model;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// for creating getter, setter, constructors, to string methods for this class
+// and reduce boilerplate code,  use Data annotation from lombok
+@Data //from Lombok library
+// to make this class as a JPA entity
+@Entity
+@Table(name = "Employees")
 public class Employee {
-	private long id;
+	@Id
 	
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;	
 }
